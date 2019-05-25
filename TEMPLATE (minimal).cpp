aad1428ad0e9ae1b681feac00b9d3da21c3e6a12
Ashok Arora <ashok.maillist@gmail.com>
2019-05-25 13:51:53 +0400

    Template

---
+++ TEMPLATE (minimal).cpp
@@ -0,0 +1,30 @@
+/**
+ *    author:  xyloid
+ *    created: python -c "import datetime;print('{:%d.%m.%y %H:%M:%S}'.format(datetime.datetime.today()));"
+ **/
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+typedef long long      ll;
+typedef pair<int, int> ii;
+typedef pair<ii, int>  iii;
+typedef vector<int>    vi;
+typedef vector<ii>     vii;
+typedef vector<iii>    viii;
+const   int            inf = 0;
+const   double         eps = 0;
+const   int            ms  = 0;
+const   int            md  = 0;
+
+
+int main(){
+        return 0;
+}
---
+++ UVa/TEMPLATE.cpp
@@ -0,0 +1,74 @@
+/**
+ *    author:  xyloid
+ *    created: python -c "import datetime;print('{:%d.%m.%y %H:%M:%S}'.format(datetime.datetime.today()));"   
+**/
+
+/* 	#include <queue>
+*	#include <list>
+*	#include <map>
+*	#include <set>
+*	#include <deque>
+*	#include <stack>
+*	#include <bitset>
+*	#include <functional>
+*	#include <numeric>
+*	#include <utility>
+*	#include <sstream>
+*	#include <iomanip>
+*	#include <ctime>
+*	#include <complex>
+*	#include <fstream>
+*	#include <cstring>
+*	#include <unordered_set>
+*	#include <unordered_map>
+*/ 
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+
+// #define endl '\n'
+#define _USE_MATH_DEFINES_
+#define mp             make_pair
+#define pb             push_back
+#define fi             first
+#define se             second
+#define sz(x)          (int)((x).size())
+#define fill(x, y)     memset(x, y, sizeof(y))
+#define all(x)         (x).begin(), (x).end()
+#define sci(x)         int x;       scanf("%d", &x);
+#define scii(x, y)     int x, y;    scanf("%d %d", &x, &y);
+#define sciii(x, y, z) int x, y, z; scanf("%d %d %d", &x, &y, &z);
+#define TC(x)          sci(x); while(x --)
+#define eprintf(...)   fprintf(stderr, __VA_ARGS__)
+#define debug(x)       { cerr << #x << " = " << x << endl; }
+#define rep(i, x, y)   for (__typeof(x) i = x; i <= y; i ++)
+#define repi(i, x, y)  for (__typeof(x) i = x; i >= y; i --)
+#define fore(itr, x)   for (__typeof(x.begin()) itr = x.begin(); itr != x.end(); itr ++)
+#define forei(itr, x)  for (__typeof(x.end()) itr = x.end() - 1; itr != x.begin() - 1; itr --)
+
+#define iis(x, y) 	   int x; string y; cin>>x>>y;
+#define ois(x, y) 	   cout<<x<<endl<<y<<endl;
+
+using namespace std;
+
+typedef long long      ll;
+typedef pair<int, int> ii;
+typedef pair<ii, int>  iii;
+typedef vector<int>    vi;
+typedef vector<ii>     vii;
+typedef vector<iii>    viii;
+const   int            inf = 0;
+const   double         eps = 0;
+const   int            ms  = 0;
+const   int            md  = 0;
+
+
+int main(){
+	return 0;	
+}
