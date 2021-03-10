62f809264073c0116bda076d719c2272914a6ff6
Ashok Arora <arora8715@gmail.com>
2021-03-10 18:48:34 +0400

    valid-parantheses different approach

--- LeetCode/20_valid-parentheses.cpp
+++ LeetCode/20_valid-parentheses.cpp
@@ -1,10 +1,12 @@
 #include <algorithm>
 #include <iostream>
 #include <stack>
+#include <unordered_map>
 #include <vector>
 
 class Solution {
   public:
+    // Using stack
     static bool isValid(std::string const &s) {
 
         if (count(s.begin(), s.end(), '(') != count(s.begin(), s.end(), ')'))
@@ -39,52 +41,43 @@ class Solution {
         }
         return true;
     }
+
+    // using vector with reserve and unordered_map
+    static bool isValid2(std::string const &s) {
+        if (0 == s.length())
+            return true;
+
+        std::vector<char> stack;
+        stack.push_back(s[0]);
+        std::unordered_map<char, char> left_to_right = {{'{', '}'}, {'[', ']'}, {'(', ')'}};
+        for (int i = 1; i < s.length(); i++) {
+            if (left_to_right[stack.back()] == s[i])
+                stack.pop_back();
+            else
+                stack.push_back(s[i]);
+        }
+        if (stack.empty())
+            return true;
+        else
+            return false;
+    }
+
+    // using stack and map
+    static bool isValid3(std::string const &s) {
+        if (s.length() == 0)
+            return true;
+        std::stack<char> sk;
+        sk.push(s[0]);
+        std::unordered_map<char, char> pairing = {{'{', '}'}, {'[', ']'}, {'(', ')'}};
+        for (int i = 1; i < s.length(); i++) { // = s.begin()+1 ; c != s.end(); c++){
+            if (s[i] == pairing[sk.top()])
+                sk.pop();
+            else
+                sk.push(s[i]);
+        }
+        if (sk.empty())
+            return true;
+        else
+            return false;
+    }
 };
-
-// class Solution {
-// public:
-//     bool isValid(string s) {
-//         if (s.length() == 0)
-//             return true;
-//         stack <char> sk;
-//         sk.push(s[0]);
-//         map<char, char> pairing = {{'{', '}'},
-//                                    {'[', ']'},
-//                                    {'(', ')'}
-//                                   };
-//         for (int i = 1 ; i < s.length(); i++){ // = s.begin()+1 ; c != s.end(); c++){
-//             if ( s[i] == pairing[sk.top()] )
-//                 sk.pop();
-//             else
-//                 sk.push(s[i]);
-//         }
-//         if (sk.empty())
-//             return true;
-//         else
-//             return false;
-//     }
-// };
-
-// class Solution {
-// public:
-//     bool isValid(string s) {
-//         if(0 == s.length())
-//             return true;
-//         vector<char> stack;
-//         stack.push_back(s[0]);
-//         std::map<char, char> left_to_right = {{'{', '}'},
-//                                               {'[', ']'},
-//                                               {'(', ')'}};
-//         for(int i = 1;i < s.length();i++)
-//         {
-//             if(left_to_right[stack.back()] == s[i])
-//                 stack.pop_back();
-//             else
-//                 stack.push_back(s[i]);
-//         }
-//         if(stack.empty())
-//             return true;
-//         else
-//             return false;
-//     }
-// };
