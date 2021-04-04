7e7199ade4e5f7b16eb46870ae916039a82cc758
Ashok Arora <arora8715@gmail.com>
2021-04-04 23:46:13 +0400

    contains duplicate

--- LeetCode/11_container-with-most-water.cpp
+++ LeetCode/11_container-with-most-water.cpp
@@ -1,31 +1,29 @@
 class Solution {
-public:
-    
+  public:
     // o(n^2) not good enough, results in TLE
-    int maxArea2(vector<int>& height) {
+    int maxArea2(vector<int> &height) {
         int max = 0;
-        for (int i = 0 ; i < height.size();i++){
-            for (int j = i+1 ; j<height.size();j++){
-                if (abs(i-j)*(min(height[i], height[j])) > max){
-                    max = abs(i-j)*(min(height[i], height[j]));
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i + 1; j < height.size(); j++) {
+                if (abs(i - j) * (min(height[i], height[j])) > max) {
+                    max = abs(i - j) * (min(height[i], height[j]));
                 }
             }
         }
         return max;
     }
 
-    int maxArea(vector <int>& height) {
+    int maxArea(vector<int> &height) {
         int maxArea = 0;
         int l = 0;
-        int r = height.size()-1;
+        int r = height.size() - 1;
         while (l < r) {
-            maxArea = max(maxArea, abs(l-r)*(min(height[l], height[r])));
-            
+            maxArea = max(maxArea, abs(l - r) * (min(height[l], height[r])));
+
             // move the smaller index
-            if (height[l] < height[r]){
+            if (height[l] < height[r]) {
                 l++;
-            }
-            else {
+            } else {
                 r--;
             }
         }
---
+++ LeetCode/217_contains-duplicate.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+class Solution {
+  public:
+
+    // o(n logn)
+    bool containsDuplicate(std::vector<int> &nums) {
+        int n = nums.size() - 1;
+        std::sort(nums.begin(), nums.begin() + n);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // o(n)
+    bool containsDuplicateHash(std::vector<int> &nums) {
+        std::unordered_set<int> uset;
+
+        for (auto x : nums) {
+            if (uset.find(x) != uset.end()) return true;
+            uset.insert(x);
+        }
+        return false;
+    }
+
+    // Note: For certain test cases with not very large n, the runtime of o(n) method can be slower than o(n logn)
+    //       due to large overhead of a hash set. - Source: Leetcode
+};
